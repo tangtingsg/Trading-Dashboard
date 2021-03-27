37f5e2d2adf0ced430b1455f7d08c2c3de8fe674
tang.ting <tangtingsg@gmail.com>
2021-03-28 01:27:13 +0800

    update test case

--- src/app/trading-dashboard/trading-dashboard.spec.ts
+++ src/app/trading-dashboard/trading-dashboard.spec.ts
@@ -14,22 +14,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create trading dashboard', () => {
     const fixture = TestBed.createComponent(TradingDashboardComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular'`, () => {
-    const fixture = TestBed.createComponent(TradingDashboardComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(TradingDashboardComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular app is running!');
   });
 });
